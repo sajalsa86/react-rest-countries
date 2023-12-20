32d75ea357fd80faa8ef6c192b7d0e9833cc974f
sajalsa86 <sajalsm86@gmail.com>
2023-12-20 08:43:56 +0600

    header add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div>
             <nav>
-                <h1>Welcome to  my Rest Countries</h1>
+                <h1>Welcome to  my Rest Countries.</h1>
                 <ul>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
