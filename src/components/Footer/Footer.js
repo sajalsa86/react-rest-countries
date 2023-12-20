3a34c75b36980fd10e5593cda5877b27263f9db6
sajalsa86 <sajalsm86@gmail.com>
2023-12-20 08:11:22 +0600

    last updated

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            Have questions or feedback? Our dedicated support team is ready to assist you. Reach out anytime through [sajalsm86@gmail.com] or give us a call at [01746233086].
+        </div>
+    );
+};
+
+export default Footer;
