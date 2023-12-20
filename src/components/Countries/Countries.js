ae0d6a8f4458770c5daef5d243d4479817372a2f
sajalsa86 <sajalsm86@gmail.com>
2023-12-20 07:29:06 +0600

    last updated

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -14,7 +14,7 @@ const Countries = () => {
     return (
         <div>
             <h1 style={{ color: 'orange' }}>Welcome to Rest Countries: {countries.length}</h1>
-            <p style={{ color: 'blue' }}>Sajal Hossain</p>
+
             <div className='countries'>
                 {countries.map(country => (
                     <Country key={country.name.common} country={country}></Country>
